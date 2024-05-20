9c4a7e1bb49cab026fe004cf58555b153be3dfdf
Daniel <idowudanielcsc190400@gmail.com>
2024-05-20 06:41:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Job salary={90000} position="Senior DEV" company="Amazon" />
+      <Job salary={60000} position="Junior DEV" company="Google" />
+      <Job salary={20000} position="Project Manager" company="Netflix" />
     </div>
   );
 }
 
+const Job = (props) => {
+return(
+  <div>
+    <h1>{props.salary}</h1>
+    <h1>{props.position}</h1>
+    <h1>{props.company}</h1>
+  </div>
+);
+};
 export default App;
