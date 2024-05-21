4d1e0898ccd67caeda156dbab903bbb7b5b5ff9c
Daniel <idowudanielcsc190400@gmail.com>
2024-05-20 17:31:45 -0700

    array first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 
-
+// list or working with array in react
 function App() {
+  const name = ["Daniel", "David","Precious", "Queen" ];
+
   return (
     <div className="App">
-      <Job salary={90000} position="Senior DEV" company="Amazon" />
-      <Job salary={60000} position="Junior DEV" company="Google" />
-      <Job salary={20000} position="Project Manager" company="Netflix" />
+      {name.map((name, key) =>{
+      return <h1 key={key}>{name}</h1>
+ } )}
+
     </div>
   );
 }
 
-const Job = (props) => {
-return(
-  <div>
-    <h1>{props.salary}</h1>
-    <h1>{props.position}</h1>
-    <h1>{props.company}</h1>
-  </div>
-);
-};
+
 export default App;
